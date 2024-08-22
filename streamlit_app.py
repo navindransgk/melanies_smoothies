24de4c2cd9cc14313e90eb78e460edf41bd1e9b0
navindransgk <naveenkumarg@gmail.com>
2024-08-22 10:57:32 +0530

    Update streamlit_app.py
    Changed fruityvice display order

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("My Parents New Healthy Diner")
@@ -34,8 +35,8 @@ if ingredients_list:
 
     for fruits_chosen in ingredients_list:
         ingredients_string += fruits_chosen + ' '
-
-    #st.write(ingredients_string)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fruityvice_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     insert_smoothie_fruit_orders = """insert into smoothies.public.orders(ingredients, name_on_order)
     VALUES ('""" + ingredients_string + """', '""" + name_on_order + """')"""
@@ -48,8 +49,3 @@ if ingredients_list:
         session.sql(insert_smoothie_fruit_orders).collect()
         
         st.success('Your Smoothie is ordered!', icon="✅")
-        
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fruityvice_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
